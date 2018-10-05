f331f8aca71b9391ad89e51b7c998e2849799695
YogaMaulanaPutra <43512249+YogaMaulanaPutra@users.noreply.github.com>
2018-10-05 10:25:13 +0700

    komentar.php
    Yoga Maulana Putra
    6701174135
    d3mi4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,19 @@
-<?php
+<form>
+	komentar :<input type="text" name="komentar">
+	<input type="submit" name="submit" value="cek">
+</form>
 
-?>
+<?php 
+		if (isset($_POST['submit'])) {
+			# code...
+			$komentar=$_POST['komentar'];
+
+			if(str_word_count($komentar)>=5){
+				echo $komentar;
+			}else if (str_word_count($komentar)<5) {
+				# code...
+				echo"erro, sangat kurang banget kata 1- 5";
+			}
+		}
+
+ ?>
